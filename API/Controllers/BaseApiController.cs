4ceb0a604daabd3ac1b8460fb8b33a36f0638cc6
Andy Martin <andy.m@genolis.com.au>
2025-07-16 15:41:57 +0800

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //localhost:5002/api
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
